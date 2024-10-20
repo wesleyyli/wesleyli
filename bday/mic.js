f3bc946bce77ebf8c0ca7ead4c2a18ff283fafe9
Wesley Li <orangewesli@gmail.com>
2024-10-20 14:42:27 -0700

    Updated info - candle WIP

---
+++ bday/mic.js
@@ -0,0 +1,32 @@
+window.onload=function(){
+    const heading = document.getElementById("start-button");
+    heading?.addEventListener("click", init);
+
+    async function init() {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+        const audioContext = new AudioContext();
+        const mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(stream);
+        const analyserNode = audioContext.createAnalyser();
+        mediaStreamAudioSourceNode.connect(analyserNode);
+
+        const pcmData = new Float32Array(analyserNode.fftSize);
+        const onFrame = () => {
+            analyserNode.getFloatTimeDomainData(pcmData);
+            let sumSquares = 0.0;
+            for (const amplitude of pcmData) { sumSquares += amplitude*amplitude; }
+            let volume = Math.sqrt(sumSquares / pcmData.length);
+            document.getElementById("volumeMeter").value = volume;
+            if(volume > 0.2) {
+                document.getElementById("blow").textContent = "true";
+                document.getElementById("candle1").classList.add("flame-out");
+            } 
+            window.requestAnimationFrame(onFrame);
+        };
+        window.requestAnimationFrame(onFrame);
+    }
+
+    async function test() {
+        alert('Test');
+        
+    }
+}
